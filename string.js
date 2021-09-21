862df7c7b1b64b19b5893bc0e46d9e25df150ba8
J. J. YOUNG <41083436+himslf@users.noreply.github.com>
2021-09-20 20:52:18 -0400

    Obligatory Save/Upload

--- string.js
+++ string.js
@@ -6,12 +6,19 @@
 // DrEvil(1000000): 1000000 dollars (pinky)
 // answer below:
 
+function DrEvil (amount) {
+	if (amount == 1000000) {
+	console.log(amount + " dollars! (pinky)");
+	} else {
+		console.log(amount + " dollars.");
+	}
+};
+
+DrEvil(1421094); // returns 1421094 dollars.
+DrEvil(1000000); // 1000000 dollars! (pinky)
 
 
-
-
-
-
+console.log("---------+---------")
 //Create a function called mixUp
 //It should take in two stings, and return the concatenation of the two strings(separated by a space)
 //slicing out and swapping the first 2 characters of each. You can assume that the strings are at least 2 characters long. 
@@ -20,22 +27,31 @@
 //mixUp('dog', 'dinner'): 'dig donner'
 //write answer below 
 
+function mixUp(word1, word2) {
+	return word2.slice(0, 2) + word1.slice(2) + " " + word1.slice(0, 2) + word2.slice(2);
+};
+
+console.log(mixUp("Knowledge", "House"));
 
 
-
-
-
+console.log("---------+---------")
 //Create a function called fixStart
 //It should take a single argument, a string, and return a version where all occurences of its first
 //character have been replaced with '*', except for the character itself. 
 //fixstart('babble'): 'ba**le'
 //write answer below
 
+function fixStart (leString) {
+	// establishing 'exclusive' letter
+	let karac = leString.charAt(0);
+	return karac + leString.slice(1).replace(new RegExp(karac, 'g'), '*');
+}
+
+console.log(fixStart("JumpingJahasfatsJeeWillikers"));
+// i had to look up the RegExp object
 
 
-
-
-
+console.log("---------+---------")
 //Create a function called verbing. It should take a single argument, a string.
 //If it's length is at least 3, it should add 'ing' to its end, unless it already ends in 'ing',
 //in which case it should add 'ly' instead. If the string length is less than 3, 
