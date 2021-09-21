e47116b490e3f47ab2796d2c908f93e346b2771d
TKH Student 36 <contactbrandos@gmail.com>
2021-09-20 22:50:22 -0400

    Lab Assignment

--- string.js
+++ string.js
@@ -71,7 +71,18 @@ function fixStart(str) {
 //write answer below
 
 
-
+function verbing(word){
+    if (word.length < 3) 
+    return word;
+    if(word.slice((-3) == 'ing')){
+        return word + 'ly';
+    } else{
+        return word + 'ing'
+    }
+}
+console.log(verbing('swim'))
+console.log(verbing('swimming'))
+console.log('go');
 
 
 
